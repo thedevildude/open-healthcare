b7b6f57986b4a02a370578e9d29749cd01e8dd0b
Devdeep Ghosh <devarmforce01@gmail.com>
2023-04-02 12:50:52 +0530

    hospitalpanel feature added

--- models/overview.js
+++ models/overview.js
@@ -23,6 +23,63 @@ module.exports = (sequelize, DataTypes) => {
         ventilator
       })
     }
+
+    static async addOccupancy({ occupiedGeneralBed, occupiedIcuBed, occupiedVentilator, hospitalID }) {
+      return this.create({
+        hospitalID,
+        occupiedGeneralBed,
+        occupiedIcuBed,
+        occupiedVentilator
+      })
+    }
+
+    static async updateGeneralBed({ generalBed, hospitalID }) {
+      return this.update({generalBed}, {
+        where: {
+          hospitalID
+        }
+      })
+    }
+
+    static async updateIcuBed({ icuBed, hospitalID }) {
+      return this.update({icuBed}, {
+        where: {
+          hospitalID
+        }
+      })
+    }
+
+    static async updateVentilator({ ventilator, hospitalID }) {
+      return this.update({ventilator}, {
+        where: {
+          hospitalID
+        }
+      })
+    }
+
+    static async updateOccupiedGeneralBed({ occupiedGeneralBed, hospitalID }) {
+      return this.update({occupiedGeneralBed}, {
+        where: {
+          hospitalID
+        }
+      })
+    }
+
+    static async updateOccupiedIcuBed({ occupiedIcuBed, hospitalID }) {
+      return this.update({occupiedIcuBed}, {
+        where: {
+          hospitalID
+        }
+      })
+    }
+
+    static async updateOccupiedVentilator({ occupiedVentilator, hospitalID }) {
+      return this.update({occupiedVentilator}, {
+        where: {
+          hospitalID
+        }
+      })
+    }
   }
 
   Overview.init({
--- routes/index.js
+++ routes/index.js
@@ -86,7 +86,10 @@ hospitalPanel.get("/", async(request, response) => {
       overviewData: true,
       generalBed: hospitaOverview.generalBed,
       ventilator: hospitaOverview.ventilator,
-      icuBed: hospitaOverview.icuBed
+      icuBed: hospitaOverview.icuBed,
+      occupiedGeneralBed: hospitaOverview.occupiedGeneralBed,
+      occupiedIcuBed: hospitaOverview.occupiedIcuBed,
+      occupiedVentilator: hospitaOverview.occupiedVentilator
     });
   } catch (error) {
     request.flash("error", error.message);
@@ -94,9 +97,11 @@ hospitalPanel.get("/", async(request, response) => {
   }
 })
 
+/* Update Total Data */
 hospitalPanel.post("/total-data", async(request, response) => {
   try {
     const hospitaOverview = await Overview.findOne({where: {hospitalID: request.user.id}})
+    console.log(request.body);
     if (hospitaOverview == null) {
       await Overview.addTotal({
         generalBed: request.body.generalBed,
@@ -104,8 +109,49 @@ hospitalPanel.post("/total-data", async(request, response) => {
         ventilator: request.body.ventilator,
         hospitalID: request.user.id
       })
+    } else {
+      console.log("Updating total");
+      if (request.body.generalBed.length !== 0) {
+        await Overview.updateGeneralBed({generalBed: request.body.generalBed, hospitalID: request.user.id})
+      }
+      if (request.body.icuBed.length !== 0) {
+        await Overview.updateIcuBed({icuBed: request.body.icuBed, hospitalID: request.user.id})
+      }
+      if (request.body.ventilator.length !== 0) {
+        await Overview.updateVentilator({ventilator: request.body.ventilator, hospitalID: request.user.id})
+      }
     }
     request.flash("success", "Added")
+    response.redirect("/hospitalpanel")
+  } catch (error) {
+    request.flash("error", error.message);
+    console.log(error);
+  }
+})
+
+hospitalPanel.post("/occupancy-update", async(request, response) => {
+  try {
+    const hospitaOverview = await Overview.findOne({where: {hospitalID: request.user.id}})
+    if (hospitaOverview == null) {
+      await Overview.addOccupancy({
+        occupiedGeneralBed: request.body.occupiedGeneralBed,
+        occupiedIcuBed: request.body.occupiedIcuBed,
+        occupiedVentilator: request.body.occupiedVentilator,
+        hospitalID: request.user.id
+      })
+    } else {
+      console.log("Updating occupancy");
+      if (request.body.occupiedGeneralBed.length !== 0) {
+        await Overview.updateOccupiedGeneralBed({occupiedGeneralBed: request.body.occupiedGeneralBed, hospitalID: request.user.id})
+      }
+      if (request.body.occupiedIcuBed.length !== 0) {
+        await Overview.updateOccupiedIcuBed({occupiedIcuBed: request.body.occupiedIcuBed, hospitalID: request.user.id})
+      }
+      if (request.body.occupiedVentilator.length !== 0) {
+        await Overview.updateOccupiedVentilator({occupiedVentilator: request.body.occupiedVentilator, hospitalID: request.user.id})
+      }
+    }
+    request.flash("success", "Updated Occupancy")
     response.redirect("/hospitalpanel")
   } catch (error) {
     request.flash("error", error.message);
